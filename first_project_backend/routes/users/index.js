db75d0643797bcc87bf744fd6e9f9d92ebf4bb24
name <min3560@gmail.com>
2021-12-15 18:41:02 +0900

    제발

--- first_project_backend/routes/users/index.js
+++ first_project_backend/routes/users/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../../controllers/users/usersController");
 const authModule = require("../../modules/authModule");
+const upload = require("../../modules/awsUpload");
 
 //계정 정보를 가져온다
 //http method : GET
 // /users
 
 router.get("/", authModule.loggedIn, usersController.getUserInfo);
-
-//resource
+router.post("/images", upload.single("img"), usersController.uploadImage);
 
 module.exports = router;
